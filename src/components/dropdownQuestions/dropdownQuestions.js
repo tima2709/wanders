b5328eb6c6b57c016d00afc586cff0869167b930
tmrln2709 <tmrln2709@gmail.com>
2023-09-18 23:17:25 +0600

    home page done

---
+++ src/components/dropdownQuestions/dropdownQuestions.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import dropdownStyle from './dropdownQ.module.css'
+import PlusIcon from "../../components/icons/plusIcon";
+import MinusIcon from "../../components/icons/minuseIcon";
+
+const DropdownQ = ({question}) => {
+
+    const [dropdownState, setDropdownState] = useState(false);
+
+    const handleDropdownClick = () => {
+        setDropdownState(!dropdownState);
+    };
+
+    return (
+        <div className={dropdownStyle.dropdown}>
+            <button onClick={handleDropdownClick} className={dropdownStyle.question}>
+                {question} {dropdownState ? <MinusIcon/> : <PlusIcon/>}
+            </button>
+            <div
+                className={`${dropdownStyle.dropdown_items} ${
+                    dropdownState ? dropdownStyle.isVisible : dropdownStyle.isHidden
+                }`}
+            >
+                <div className={dropdownStyle.dropdown_item}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet et labore quidem sequi
+                        temporibus. Assumenda earum est et fugiat nihil, odit omnis optio quasi ratione rem
+                        sapiente sit voluptate voluptates?</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default DropdownQ;
