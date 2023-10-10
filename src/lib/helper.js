8d9bbf4afff6c3d9800fe06293ea27bacaffe5ff
BaQuna <imbaquna@gmail.com>
2023-10-10 15:49:48 +0600

    test commit

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie'
 export const isAuth = () => {
+    
     // const isAuthenticated = JSON.parse(localStorage.getItem('isAuth'))
     const token = Cookies.get('token')
     const localToken = JSON.parse(localStorage.getItem('token'))
